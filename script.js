e5ce420b09b813f7dc9e8e5794bc44569cfe45be
webdevkush <134157044+webdevkush@users.noreply.github.com>
2023-07-17 18:24:05 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
+const share = document.getElementsByClassName('.share');
 
+setTimeout(() => {
+  share.classList.add("hover");
+}, 1000);
+
+setTimeout(() => {
+  share.classList.remove("hover");
+}, 3000);
